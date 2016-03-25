6acaed05d98b4914f12fc194db602da4829e8a23
sceone <sceone22@gmail.com>
2016-03-25 18:09:34 +0900

    v16.35.1 : Getting ready to hook

--- Core.lua
+++ Core.lua
@@ -1,6 +1,23 @@
 --------------------------------------------------
 -- Original codes : Modules/UnitFrames/Core.lua
 --------------------------------------------------
-
 local T, C, L = unpack(Tukui)
 local TukuiUnitFrames = T.UnitFrames
+
+function TukuiUnitFrames:UpdateThreat(event, unit)
+	if (not C.UnitFrames.Threat) or (unit ~= self.unit) then
+		return
+	end
+
+	local Panel = self.Panel
+	
+	if Panel then
+		local Status = UnitThreatSituation(unit)
+		
+		if Status and Status > 0 then
+			Panel:SetBackdropBorderColor(1, 0, 0)
+		else
+			Panel:SetBackdropBorderColor(C["General"].BorderColor[1] * 0.7, C["General"].BorderColor[2] * 0.7, C["General"].BorderColor[3] * 0.7)
+		end
+	end
+end
